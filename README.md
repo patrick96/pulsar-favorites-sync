6bfcad39d4c3b2811188ee250e42ebd18b4fc8c0
patrick96 <p.ziegler96@gmail.com>
2017-04-15 12:08:19 +0200

    First working version w/ instructions

---
+++ README.md
@@ -0,0 +1,62 @@
+# Pulsar Favorites Sync
+
+![GPL v3.0](https://img.shields.io/github/license/patrick96/notification-mount.svg)
+
+This script helps you synchronize your list of favorites in the [Pulsar Music Player][pulsar] with a local playlist on
+your computer.
+
+# Requirements
+
+This script was designed to work on Linux but it should work on any system that can run bash.
+
+* [sqlite3](https://www.sqlite.org/)
+* Java
+* [Android Platform Tools (for adb)](https://developer.android.com/studio/releases/platform-tools.html)
+
+Additionally if this script should be useful, you will need to have the same folder structure for your
+music files on your computer and your phone. So for any file the path relative to the music directory
+should be the same on your phone and computer
+
+# Usage
+
+First make sure you meet all the requirements described above, if you don't, things could go wrong.
+
+Now clone this repository with its submodules:
+``` bash
+git clone --recursive https://github.com/patrick96/pulsar-favorites-sync
+cd pulsar-favorites-sync
+```
+
+Connect your android phone to your computer, make sure to enable usb debugging on the phone.
+Use `adb devices` to make sure it is recognized. If you have multiple devices connected, disconnect
+all except for the one you want to synchronize (Multiple connected android devices are not supported)
+
+Now you will need to set the path to the music folder on your phone and the path to the desired playlist
+file on your computer inside the sync script.
+For that open `sync.sh` at the line starting with `MUSIC_FOLDER` replace `%INSERT PATH HERE%` with the 
+absolute path to the music folder on your phone.
+On the line starting with `FAVORITES` replace `%INSERT PATH HERE%` with the path to your local favorites
+playlist.
+
+For me this would look like this:
+``` bash
+MUSIC_FOLDER="/storage/9C33-6BBD/Music/"
+FAVORITES="$HOME/Music/Playlists/f.m3u"
+```
+
+Now you can run `./sync.sh`
+
+This will shortly after prompt you for an encryption password to export the favorites database from
+your phone, insert '1' (without the quotes) and press "BACK UP MY DATA"
+
+Now the script will merge the favorites from the specified favorites files with the ones from your
+phone. It will also create a backup of the old favorites file, just in case.
+
+The merged favorites will now be copied back onto the phone, for that your phone will ask you for a 
+decryption password, which you can leave empty.
+
+Now you should be done.
+But also check if everything was successful because your phone will not tell you if there was an error
+with syncing the favorites back to your phone, if you feel like something went wrong check the output of `adb logcat | grep "BackupManagerService"`
+
+[pulsar]: https://play.google.com/store/apps/details?id=com.rhmsoft.pulsar
---
+++ sync.sh
@@ -0,0 +1,130 @@
+#!/usr/bin/env bash
+
+usage() {
+    echo -e "Usage: ./$(basename "$0")"
+    echo -e "Description: Sync favorites from pulsar with a local playlist"
+    echo -e "Options:"
+    echo -e "    --help: Display this help message"
+    exit 0
+}
+
+expr "$*" : ".*--help" > /dev/null && usage
+
+# Root folder for your music files on your phone
+MUSIC_FOLDER="%INSERT PATH HERE%"
+FAVORITES="%INSERT PATH HERE%"
+
+pkg="com.rhmsoft.pulsar"
+wd="$(mktemp -d)"
+
+# The playlist exported from pulsar will be saved here
+exported="$wd/exported.m3u"
+merged="$wd/merged.m3u"
+
+# The old playlist will be copied to here before it is overwritten
+backup="$wd/backup.m3u"
+
+restore="$wd/restore.sql"
+
+echo "Starting syncing process in $wd"
+
+# Build abe {{{
+# TODO only build once and then copy to cache
+cd libs/android-backup-extractor || exit 1
+./gradlew
+cp build/libs/abe-all.jar "$wd"
+# }}}
+
+# Export from phone {{{
+
+cd "$wd" || exit 1
+
+echo "Creating backup please use 1 as the password"
+# Set password to 1
+adb backup -noapk "$pkg"
+
+echo "Encrypting and extracting backup.ab"
+# Backup application data from phone, decrypt and extract it
+java -jar abe-all.jar unpack "$wd/backup.ab" "$wd/backup.tar" "1"
+tar xf backup.tar
+cd "apps/$pkg/db" || exit 1
+
+echo "Exporting pulsar favorites to $exported"
+
+# Read the absolute paths of the favorites from the database
+# Remove the root music folder from the paths to get the relative paths
+# Finally remove leading slashes from the paths, if $MUSIC_FOLDER doesn't end with a slash
+sqlite3 player.db 'select data from favorites;' | sed "s|^$MUSIC_FOLDER||" | sed 's/^\///' > "$exported"
+
+cd ../../../ || exit 1
+
+# Append local favorites to the exported ones
+cat "$FAVORITES" >> "$exported"
+
+# Remove empty lines
+sed -i '/^$/d' "$exported"
+
+# Remove duplicates
+awk '!x[$0]++' "$exported" > "$merged"
+
+# Backup old favorites file
+cp "$FAVORITES" "$backup"
+echo "Old favorites were successfully backed up to $backup"
+
+# Overwrite with new favorites
+cp "$merged" "$FAVORITES"
+
+echo -e "\e[0;32mSuccessfully merged favorites into $FAVORITES\e[0m"
+
+# }}}
+
+# Sync to phone {{{
+
+echo "BEGIN TRANSACTION;" > "$restore"
+
+while read -r f ; do
+    echo "INSERT INTO 'favorites' ('data') VALUES ('${MUSIC_FOLDER}${f//\'/\'\'}');" >> "$restore"
+done < "$FAVORITES"
+
+echo "COMMIT;" >> "$restore"
+
+cd "apps/$pkg/db" || exit 1
+
+sqlite3 player.db 'DELETE FROM favorites' || exit 1
+
+sqlite3 -batch player.db < "$restore" || exit 1
+
+cd ../../../ || exit 1
+
+# Create tar archive of the modified app data
+tar --create --file apps.tar apps || exit 1
+
+# We need to create a list of files to be included in the backup file to restore because adb restore doesn't handle
+# paths with trailing slashes (representing folders) and the _manifest entry has to be at the beginning
+# We exclude any hidden files starting with "."
+tar tf apps.tar | grep -F "$pkg" | grep -v "\/\." > package.list || exit 1
+
+# Remove lines that end with a slash (adb restore doesn't like that)
+sed -i '/\/$/d' package.list
+
+# The _manifest file needs to be at the start of the archive
+manifestLine="$(grep -F "_manifest" < package.list)"
+nonManifestLines="$(grep -v -F "_manifest" < package.list)"
+
+echo "$manifestLine" > package.list
+echo "$nonManifestLines" >> package.list
+
+# Create tar restore archive from the file list
+tar cf restore.tar -T package.list || exit 1
+
+# Repack w/o password
+java -jar abe-all.jar pack restore.tar restore.ab "" || exit 1
+
+echo "Restoring updates favorites on phone"
+echo "When prompted for a password, leave the password field empty"
+adb restore restore.ab || exit 1
+
+echo "Restore finished"
+echo "Please check if the data was successfully restored to the phone, if not check for errors with"
+echo -e "    \e[1;31madb logcat | grep \"BackupManagerService\"\e[0m"
+# }}}
